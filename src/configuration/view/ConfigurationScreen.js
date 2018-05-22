ca48fd6398123690219157427897f956ab746ef0
Victor Arruda Magalhães <victor@onlytech.me>
2018-05-22 16:42:46 -0300

    icons and foward button organized inside de box

--- src/configuration/view/ConfigurationScreen.js
+++ src/configuration/view/ConfigurationScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Text, View, StyleSheet, FlatList} from 'react-native';
 import Svg, {Path} from 'react-native-svg';
+import {Font} from 'expo';
 
 class ConfigurationScreen extends React.Component {
 
@@ -290,6 +291,18 @@ class ConfigurationScreen extends React.Component {
         ]
     };
 
+    state = {
+        fontLoaded: false,
+    };
+
+    async componentDidMount() {
+        await Font.loadAsync({
+            'montserrat-regular': require('../../../assets/fonts/Montserrat-Regular.ttf'),
+        });
+
+        this.setState({fontLoaded: true});
+    }
+
     renderPaths(item) {
         return item.paths.map((p, index) => {
             return <Path
@@ -313,18 +326,28 @@ class ConfigurationScreen extends React.Component {
                 renderItem={({item}) => {
                     return (
                         <View style={styles.item}>
-                            <Svg width={item.width} height={item.height} viewBox={item.viewBox}>
-                                {this.renderPaths(item)}
-                            </Svg>
-                            <Text style={styles.text}>
-                                {item.name}
-                            </Text>
-                            <Svg width='8' height='14' viewBox='0 0 18 32'>
-                                <Path
-                                    fill='#2424d3'
-                                    d='M13.365 15.977l-12.77 12.606c-0.367 0.36-0.594 0.86-0.594 1.414 0 0.557 0.23 1.060 0.6 1.42l0 0c0.8 0.779 2.091 0.777 2.885-0.007l14.206-14.021c0.367-0.36 0.595-0.861 0.595-1.415s-0.228-1.055-0.595-1.415l-0-0-14.208-13.973c-0.372-0.363-0.881-0.587-1.442-0.587s-1.070 0.224-1.443 0.587l0-0c-0.369 0.36-0.597 0.862-0.597 1.417s0.229 1.057 0.597 1.417l0 0 12.763 12.555z'
-                                />
-                            </Svg>
+                            <View style={styles.boxIcon}>
+                                <Svg style={styles.icon} width={item.width} height={item.height} viewBox={item.viewBox}>
+                                    {this.renderPaths(item)}
+                                </Svg>
+                            </View>
+                            <View style={styles.boxText}>
+                                {
+                                    this.state.fontLoaded ? (        
+                                        <Text style={styles.text}>
+                                            {item.name}
+                                        </Text>
+                                    ) : null
+                                }
+                            </View>
+                            <View style={styles.boxFoward}>
+                                <Svg style={styles.foward} width='8' height='14' viewBox='0 0 18 32'>
+                                    <Path
+                                        fill='#2424d3'
+                                        d='M13.365 15.977l-12.77 12.606c-0.367 0.36-0.594 0.86-0.594 1.414 0 0.557 0.23 1.060 0.6 1.42l0 0c0.8 0.779 2.091 0.777 2.885-0.007l14.206-14.021c0.367-0.36 0.595-0.861 0.595-1.415s-0.228-1.055-0.595-1.415l-0-0-14.208-13.973c-0.372-0.363-0.881-0.587-1.442-0.587s-1.070 0.224-1.443 0.587l0-0c-0.369 0.36-0.597 0.862-0.597 1.417s0.229 1.057 0.597 1.417l0 0 12.763 12.555z'
+                                    />
+                                </Svg>
+                            </View>
                         </View>
                     );
                 }}
@@ -354,6 +377,22 @@ const styles = StyleSheet.create({
             width: 1,
             height: 1
         }
+    },
+    text: {
+        color: '#CC0000',
+        fontFamily: 'montserrat-regular',
+        fontSize: 16
+    },
+    boxIcon: {
+        width: 32,
+        marginLeft: 12,
+        paddingRight: 12,
+        borderRightWidth: 2,
+        borderRightColor: '#F6F6F6',
+        alignItems: 'center'
+    },
+    boxFoward: {
+        marginRight: 14
     }
 });
 
