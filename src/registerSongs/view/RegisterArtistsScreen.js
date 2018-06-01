54b9249fd3f8d713ee76ecd5ca20e56aaec167e4
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-06-01 18:28:00 -0300

    - Updated RegisterArtistsScreen to have ScrollView

--- src/registerSongs/view/RegisterArtistsScreen.js
+++ src/registerSongs/view/RegisterArtistsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput} from 'react-native';
+import {StyleSheet, ScrollView, Text, View, TextInput} from 'react-native';
 import { Icon } from 'react-native-elements'
 import { TextField } from 'react-native-material-textfield';
 import { MPHeader, MPTextField } from '../../components'
@@ -9,29 +9,27 @@ class RegisterArtistsScreenContainer extends React.Component {
   
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <MPHeader back={true} onBack={this.handleBackClick} title={"Intérpretes"} />
         {
           this.props.fontLoaded ? (
-            <View>
-              <MPHeader back={true} onBack={this.handleBackClick} title={"Intérpretes"} />
-              <View style={styles.container}>
-                <Text style={styles.textTop}>Essa música tem intérpretes?</Text>
-                <View style={ styles.textFieldWithButtonContainer}>
-                    <TextField label="Pesquise pelo nome"
-                    value=""
-                    baseColor="#b1b1b1"
-                    labelFontSize={16} 
-                    lineWidth={1}
-                    containerStyle={{flex: 1}}
-                    labelTextStyle={{ fontFamily: 'montSerrat' }}
-                    titleTextStyle={{ fontFamily: 'montSerrat' }}/>
-                    <Icon name='search' color='#e13223' size={20} containerStyle={ styles.textFieldIcon }/>
-                </View>
-                <View style={styles.clickableTextContainer}>
-                  <Text style={styles.clickableText}>Não, apenas eu</Text>
-                </View>
+            <ScrollView>
+              <Text style={styles.textTop}>Essa música tem intérpretes?</Text>
+              <View style={ styles.textFieldWithButtonContainer}>
+                  <TextField label="Pesquise pelo nome"
+                  value=""
+                  baseColor="#b1b1b1"
+                  labelFontSize={16} 
+                  lineWidth={1}
+                  containerStyle={{flex: 1}}
+                  labelTextStyle={{ fontFamily: 'montSerrat' }}
+                  titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+                  <Icon name='search' color='#e13223' size={20} containerStyle={ styles.textFieldIcon }/>
               </View>
-            </View>
+              <View style={styles.clickableTextContainer}>
+                <Text style={styles.clickableText}>Não, apenas eu</Text>
+              </View>
+            </ScrollView>
           ) : null
         }
       </View>
@@ -41,30 +39,34 @@ class RegisterArtistsScreenContainer extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 30,
-    marginBottom: 30,
-    marginStart: 40,
-    marginEnd: 40,
-    flexDirection: 'column',
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#FCFCFC',
+    justifyContent: 'flex-end'
   },
   textTop: {
     fontSize: 16,
     color: '#686868',
     height: 20,
     marginBottom: 20,
+    marginTop: 30,
+    marginStart: 40,
+    marginEnd: 40,
     fontFamily: 'montSerrat'
   },
   textFieldWithButtonContainer: {
       flexDirection: 'row',
       padding: 0,
+      marginStart: 40, 
+      marginEnd: 40,
   },
   textFieldIcon: {
       alignSelf: 'flex-end',
       paddingBottom: 16,
   },
   clickableTextContainer: {
-    alignItems: 'center',
-    height: 20
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
   clickableText: {
     borderBottomWidth: 1,
