8f3173c20cc213a43f9bc2d4f4e5e9c0e89dc37a
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-06-02 10:31:48 -0300

    - Updated UploadMediaEmpty to use font, header and SCrollview;

--- src/registerSongs/view/UploadMediaEmptyScreen.js
+++ src/registerSongs/view/UploadMediaEmptyScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Alert, StyleSheet, Text, View, TextInput, FlatList} from 'react-native';
+import { Alert, StyleSheet, Text, View, TextInput, FlatList, ScrollView } from 'react-native';
 import { Icon } from 'react-native-elements'
-import { MPGradientButton } from '../../components'
+import { MPGradientButton, MPHeader } from '../../components'
 import { TextField } from 'react-native-material-textfield';
 import { connect } from 'react-redux';
 
@@ -14,95 +14,120 @@ class UploadMediaEmptyScreenContainer extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={ styles.topIndicator}>
-          <View style={ styles.topIndicatorDone}></View>
-          <View style={ styles.topIndicatorLeft}></View>
-        </View>
-        <Text style={styles.headerTitle}>Mostre pra todo mundo o que você faz de melhor.</Text>
-        <Text style={ styles.headerText}>Upload de melodia</Text>
-        <MPGradientButton iconName={'music-tone-alt'} iconType={'simple-line-icon'} title='Escolher o arquivo' onPress={ () => {} } textSize={16} style={ {marginBottom: 10} } />
-        <Text style={ styles.subText}>Você pode fazer upload de músicas em MP3 ou AAC.</Text>
-        <View style={ styles.textFieldsVerticalContainer }>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              titleTextStyle={{color: '#000'}}
-              lineWidth={0}
-              label='Qual é o título da música?'
-              value={this.props.song.name}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}/>
+        <MPHeader back={true} onBack={this.handleBackClick} title={"Hora de fazer sucesso"} />
+        <ScrollView style={styles.scroll}>
+        {
+          this.props.fontLoaded ? (
+            <View>
+              <View style={ styles.topIndicator}>
+                <View style={ styles.topIndicatorDone}></View>
+                <View style={ styles.topIndicatorLeft}></View>
+              </View>
+              <View style={{paddingStart: 50, paddingEnd: 50}}>
+                <Text style={styles.headerTitle}>Mostre pra todo mundo o que você faz de melhor.</Text>
+                <Text style={ styles.headerText}>Upload de melodia</Text>
+              </View>
+              <View style={{paddingStart: 20, paddingEnd: 20, paddingBottom: 20}}>
+                <MPGradientButton iconName={'music-tone-alt'} iconType={'simple-line-icon'} title='Escolher o arquivo' onPress={ () => {} } textSize={16} style={ {marginBottom: 10} } />
+                <Text style={ styles.subText}>Você pode fazer upload de músicas em MP3 ou AAC.</Text>
+                <View style={ styles.textFieldsVerticalContainer }>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      titleTextStyle={{color: '#000'}}
+                      lineWidth={0}
+                      label='Qual é o título da música?'
+                      value={this.props.song.name}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      labelTextStyle={{ fontFamily: 'montSerrat' }}
+                      titleTextStyle={{ fontFamily: 'montSerrat' }} />
+                    </View>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      labelTextStyle={{color: '#000'}}
+                      lineWidth={0}
+                      label='Qual é a letra?'
+                      value={this.props.song.letter}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      labelTextStyle={{ fontFamily: 'montSerrat' }}
+                      titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+                    </View>
+                  </View>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Quais as categorias e estilos que combinam?'
+                      value={this.props.song.genres}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      labelTextStyle={{ fontFamily: 'montSerrat' }}
+                      titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+                    </View>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Fale um pouquinho mais sobre a música'
+                      value={this.props.song.description}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      labelTextStyle={{ fontFamily: 'montSerrat' }}
+                      titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+                    </View>
+                  </View>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Tem outros autores?'
+                      value={this.props.song.authors}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      labelTextStyle={{ fontFamily: 'montSerrat' }}
+                      titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+                    </View>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Tem outros intérpretes?'
+                      value={this.props.song.interpreters}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      labelTextStyle={{ fontFamily: 'montSerrat' }}
+                      titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+                    </View>
+                  </View>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Organize sua música em pastas'
+                      value={this.props.song.folders}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      labelTextStyle={{ fontFamily: 'montSerrat' }}
+                      titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+                    </View>
+                  </View>
+                </View>
+                <MPGradientButton title='Publicar' onPress={ () => {} } textSize={16} style={ {marginBottom: 20} } />
+                <View style={styles.clickableTextContainer}>
+                  <Text style={styles.clickableText} >Terminar depois</Text>
+                </View> 
+              </View>
             </View>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              labelTextStyle={{color: '#000'}}
-              lineWidth={0}
-              label='Qual é a letra?'
-              value={this.props.song.letter}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}/>
-            </View>
-          </View>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Quais as categorias e estilos que combinam?'
-              value={this.props.song.genres}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}/>
-            </View>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Fale um pouquinho mais sobre a música'
-              value={this.props.song.description}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              labelTextStyle={{flex: 1,flexWrap: 'wrap'}}
-              style={{fontSize: 10}}/>
-            </View>
-          </View>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Tem outros autores?'
-              value={this.props.song.authors}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}/>
-            </View>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Tem outros intérpretes?'
-              value={this.props.song.interpreters}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}/>
-            </View>
-          </View>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Organize sua música em pastas'
-              value={this.props.song.folders}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}/>
-            </View>
-          </View>
-        </View>
-        <MPGradientButton title='Publicar' onPress={ () => {} } textSize={16} style={ {marginBottom: 20} } />
-        <View style={styles.clickableTextContainer}>
-          <Text style={styles.clickableText} >Terminar depois</Text>
-        </View> 
+          ) : null
+        }
+        </ScrollView>
       </View>
     );
   }
@@ -110,12 +135,13 @@ class UploadMediaEmptyScreenContainer extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    marginTop: 30,
-    marginBottom: 30,
-    marginStart: 20,
-    marginEnd: 20,
-    flexDirection: 'column'
+    backgroundColor: '#FCFCFC',
+    justifyContent: 'flex-end'
+  },
+  scroll: {
+    flex: 2,
   },
   topIndicator: {
     height: 7,
@@ -143,6 +169,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     textAlign: 'center',
     marginBottom: 20,
+    fontFamily: 'montSerrat',
+    fontWeight: 'bold'
   },
   headerText: {
     fontSize: 16,
@@ -150,12 +178,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     textAlign: 'center',
     marginBottom: 10,
+    fontFamily: 'montSerrat',
+    fontWeight: 'bold'
   },
   subText: {
     fontSize: 12,
     color: '#686868',
     alignItems: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    fontFamily: 'montSerrat',
   },
   clickableTextContainer: {
     alignItems: 'center',
@@ -167,6 +198,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#5994db',
     fontSize: 14,
+    fontFamily: 'montSerrat',
   },
   textFieldsVerticalContainer: {
     flex: 1,
