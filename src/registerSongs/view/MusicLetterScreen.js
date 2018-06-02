71d423e189d34cd4a666aa760cd60f558315cce1
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-06-02 10:03:29 -0300

    - Updated MusicLetterScreen to use font, header and ScrollView; - MPSelect not yet inserted in MusicLetterScreen;

--- src/registerSongs/view/MusicLetterScreen.js
+++ src/registerSongs/view/MusicLetterScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput, Picker} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Picker, ScrollView } from 'react-native';
 import { TextField } from 'react-native-material-textfield';
 import { connect } from 'react-redux';
 import {updateSongRegisterData} from '../../state/action';
+import { MPHeader } from '../../components';
 
 class MusicLetterScreenContainer extends React.Component {
   constructor(props){
@@ -19,31 +20,40 @@ class MusicLetterScreenContainer extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textTop}>Pode colar a letra da música aqui:</Text>
-        <TextField 
-        label='Letra da Música'
-        onChangeText={this.handleChangeLetter}
-        value={this.props.song.letter}
-        labelFontSize={16}
-        multiline={true}
-        lineWidth={1}
-        baseColor={'#b1b1b1'} />
-        <View style={[styles.clickableTextContainer, {marginTop: 20}]}>
-          <Text>ou </Text>
-          <Text style={styles.clickableText}>faça upload da letra(doc, tx ou rtf)</Text>
-        </View>
-        <View style={ styles.pickerContainer }>
-          <Picker
-              selectedValue={this.state.language}
-              onValueChange={(itemValue, itemIndex) => itemValue != 'default' ? this.setState({language: itemValue}) : {}}
-              mode={'dropdown'}
-              style={ {color: "#b1b1b1"} }>
-              <Picker.Item label="Idioma" value="default" style={styles.notEnabled}  />
-              <Picker.Item label="Português" value="pt" />
-              <Picker.Item label="Inglês" value="en" />
-              <Picker.Item label="Espanhol" value="es" />
-          </Picker>
-        </View>
+        <MPHeader back={true} onBack={this.handleBackClick} title={"Letra da música"} />
+        {
+          this.props.fontLoaded ? (
+            <ScrollView style={styles.scroll}>
+              <Text style={styles.textTop}>Pode colar a letra da música aqui:</Text>
+              <TextField 
+              label='Letra da Música'
+              onChangeText={this.handleChangeLetter}
+              value={this.props.song.letter}
+              labelFontSize={16}
+              multiline={true}
+              lineWidth={1}
+              baseColor={'#b1b1b1'} 
+              labelTextStyle={{ fontFamily: 'montSerrat' }}
+              titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+              <View style={[styles.clickableTextContainer, {marginTop: 20}]}>
+                <Text style={{fontFamily: 'montSerrat'}}>ou </Text>
+                <Text style={styles.clickableText}>faça upload da letra(doc, tx ou rtf)</Text>
+              </View>
+              <View style={ styles.pickerContainer }>
+                <Picker
+                    selectedValue={this.state.language}
+                    onValueChange={(itemValue, itemIndex) => itemValue != 'default' ? this.setState({language: itemValue}) : {}}
+                    mode={'dropdown'}
+                    style={ {color: "#b1b1b1"} }>
+                    <Picker.Item label="Idioma" value="default" style={styles.notEnabled}  />
+                    <Picker.Item label="Português" value="pt" />
+                    <Picker.Item label="Inglês" value="en" />
+                    <Picker.Item label="Espanhol" value="es" />
+                </Picker>
+              </View>
+            </ScrollView>
+          ) : null
+        }
       </View>
     );
   }
@@ -51,17 +61,22 @@ class MusicLetterScreenContainer extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    marginTop: 30,
-    marginBottom: 30,
-    marginStart: 40,
-    marginEnd: 40,
-    flexDirection: 'column'
+    backgroundColor: '#FCFCFC',
+    justifyContent: 'flex-end'
+  },
+  scroll: {
+    flex: 2,
+    paddingTop: 30,
+    paddingStart: 40,
+    paddingEnd: 40
   },
   textTop: {
     fontSize: 16,
     color: '#686868',
     marginBottom: 20,
+    fontFamily: 'montSerrat'
   },
   textInputContainer: {
     height: 46,
@@ -76,7 +91,7 @@ const styles = StyleSheet.create({
   clickableTextContainer: {
     alignItems: 'flex-start',
     flexDirection: 'row',
-    height: 20
+    height: 20,
   },
   clickableText: {
     borderBottomWidth: 1,
@@ -84,6 +99,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#5994db',
     fontSize: 14,
+    fontFamily: 'montSerrat'
   },
   pickerContainer: {
     height: 46,
@@ -96,8 +112,8 @@ const styles = StyleSheet.create({
   }
 });
 
-const mapStateToProps = ({songsReducer}) => {
-  return {...songsReducer}
+const mapStateToProps = ({fontReducer, songsReducer}) => {
+  return {...fontReducer, ...songsReducer}
 };
 
 const MusicLetterScreen = connect(mapStateToProps)(MusicLetterScreenContainer);
