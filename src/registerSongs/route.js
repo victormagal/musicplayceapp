89b7f3308ee22aa95aea1a055c6e5c711cafa698
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-06-02 10:40:06 -0300

    - Updated UploadMediaFilledScreen to use font, header and scrollview;

--- src/registerSongs/route.js
+++ src/registerSongs/route.js
@@ -20,7 +20,7 @@ export const SongsScreensNavigation = createStackNavigator(
   {
     UploadMediaEmptyScreen: {
       // screen: TermsConfigurationScreen,
-      screen: UploadMediaEmptyScreen,
+      screen: UploadMediaFilledScreen,
       navigationOptions: {
         header: null
       }
--- src/registerSongs/view/UploadMediaFilledScreen.js
+++ src/registerSongs/view/UploadMediaFilledScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {StyleSheet, Text, View, TextInput, FlatList} from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, ScrollView } from 'react-native';
 import { Icon } from 'react-native-elements'
-import { MPGradientButton } from '../../components'
+import { MPGradientButton, MPHeader } from '../../components'
 import { TextField } from 'react-native-material-textfield';
+import { connect } from 'react-redux';
 
-class UploadMediaFilledScreen extends React.Component {
+class UploadMediaFilledScreenContainer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
@@ -21,111 +22,124 @@ class UploadMediaFilledScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={ styles.topIndicator}>
-          <View style={ styles.topIndicatorDone}></View>
-          <View style={ styles.topIndicatorLeft}></View>
-        </View>
-        <Text style={styles.headerTitle}>Mostre pra todo mundo o que você faz de melhor.</Text>
-        <Text style={ styles.headerText}>Melodia selecionada</Text>
-        <MPGradientButton iconName={'play-circle-o'} iconType={ 'font-awesome' } title='Nome da música.mp3' textSize={16} onPress={ () => {} } textSize={16} style={ {marginBottom: 10} } />
-        <View style={styles.clickableTextContainer}>
-          <Text style={styles.clickableText}>Substituir arquivo</Text>
-        </View> 
-        <View style={ styles.textFieldsVerticalContainer }>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              titleTextStyle={{color: '#000'}}
-              inputContainerStyle={{flexWrap: 'wrap'}}
-              lineWidth={0}
-              label='Qual é o título da música?'
-              value={this.state.titleText}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              onChangeText={(titleText) => this.setState({titleText})}/>
-              <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+        <MPHeader back={true} onBack={this.handleBackClick} title={"Hora de fazer sucesso"} />
+        <ScrollView style={styles.scroll}>
+        {
+          this.props.fontLoaded ? (
+            <View>
+              <View style={ styles.topIndicator}>
+                <View style={ styles.topIndicatorDone}></View>
+                <View style={ styles.topIndicatorLeft}></View>
+              </View>
+              <View style={{paddingStart: 50, paddingEnd: 50}}>
+                <Text style={styles.headerTitle}>Mostre pra todo mundo o que você faz de melhor.</Text>
+                <Text style={ styles.headerText}>Melodia selecionada</Text>
+              </View>
+              <View style={{paddingStart: 20, paddingEnd : 20, paddingBottom: 20}}>
+                <MPGradientButton iconName={'play-circle-o'} iconType={ 'font-awesome' } title='Nome da música.mp3' textSize={16} onPress={ () => {} } textSize={16} style={ {marginBottom: 10} } />
+                <View style={styles.clickableTextContainer}>
+                  <Text style={styles.clickableText}>Substituir arquivo</Text>
+                </View> 
+                <View style={ styles.textFieldsVerticalContainer }>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      titleTextStyle={{color: '#000'}}
+                      inputContainerStyle={{flexWrap: 'wrap'}}
+                      lineWidth={0}
+                      label='Qual é o título da música?'
+                      value={this.state.titleText}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      onChangeText={(titleText) => this.setState({titleText})}/>
+                      <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+                    </View>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      labelTextStyle={{color: '#000'}}
+                      inputContainerStyle={{flexWrap: 'wrap'}}
+                      lineWidth={0}
+                      label='Qual é a letra?'
+                      value={this.state.letterText}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      onChangeText={(letterText) => this.setState({letterText})}/>
+                      <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+                    </View>
+                  </View>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Quais as categorias e estilos que combinam?'
+                      value={this.state.stilesText}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      onChangeText={(stilesText) => this.setState({stilesText})}/>
+                      <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+                    </View>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Fale um pouquinho mais sobre a música'
+                      value={this.state.decriptionText}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      onChangeText={(decriptionText) => this.setState({decriptionText})}/>
+                      <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+                    </View>
+                  </View>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Tem outros autores?'
+                      value={this.state.authorsText}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      onChangeText={(authorsText) => this.setState({authorsText})}/>
+                      <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+                    </View>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Tem outros intérpretes?'
+                      value={this.state.interpretersText}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      onChangeText={(interpretersText) => this.setState({interpretersText})}/>
+                      <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+                    </View>
+                  </View>
+                  <View style={  styles.textFieldsHorizontalContainer}>
+                    <View style={ styles.textFieldsInnerContainer}>
+                      <TextField
+                      lineWidth={0}
+                      label='Organize sua música em pastas'
+                      value={this.state.folderText}
+                      labelFontSize={12}
+                      baseColor={'#000'}
+                      textColor={'#686868'}
+                      onChangeText={(folderText) => this.setState({folderText})}/>
+                      <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
+                    </View>
+                  </View>
+                </View>
+                <MPGradientButton title='Publicar' onPress={ () => {} } textSize={16} style={ {marginBottom: 20} } />
+                <View style={styles.clickableTextContainer}>
+                  <Text style={styles.clickableText}>Terminar depois</Text>
+                </View> 
+              </View>
             </View>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              labelTextStyle={{color: '#000'}}
-              inputContainerStyle={{flexWrap: 'wrap'}}
-              lineWidth={0}
-              label='Qual é a letra?'
-              value={this.state.letterText}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              onChangeText={(letterText) => this.setState({letterText})}/>
-              <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
-            </View>
-          </View>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Quais as categorias e estilos que combinam?'
-              value={this.state.stilesText}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              onChangeText={(stilesText) => this.setState({stilesText})}/>
-              <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
-            </View>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Fale um pouquinho mais sobre a música'
-              value={this.state.decriptionText}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              onChangeText={(decriptionText) => this.setState({decriptionText})}/>
-              <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
-            </View>
-          </View>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Tem outros autores?'
-              value={this.state.authorsText}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              onChangeText={(authorsText) => this.setState({authorsText})}/>
-              <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
-            </View>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Tem outros intérpretes?'
-              value={this.state.interpretersText}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              onChangeText={(interpretersText) => this.setState({interpretersText})}/>
-              <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
-            </View>
-          </View>
-          <View style={  styles.textFieldsHorizontalContainer}>
-            <View style={ styles.textFieldsInnerContainer}>
-              <TextField
-              lineWidth={0}
-              label='Organize sua música em pastas'
-              value={this.state.folderText}
-              labelFontSize={12}
-              baseColor={'#000'}
-              textColor={'#686868'}
-              onChangeText={(folderText) => this.setState({folderText})}/>
-              <Icon name='check-circle' color='#f00' size={18} containerStyle={ styles.stretchedArtistSelectedIcon }/>
-            </View>
-          </View>
-        </View>
-        <MPGradientButton title='Publicar' onPress={ () => {} } textSize={16} style={ {marginBottom: 20} } />
-        <View style={styles.clickableTextContainer}>
-          <Text style={styles.clickableText}>Terminar depois</Text>
-        </View> 
+          ) : null
+        }
+        </ScrollView>
       </View>
     );
   }
@@ -133,12 +147,13 @@ class UploadMediaFilledScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    marginTop: 30,
-    marginBottom: 30,
-    marginStart: 20,
-    marginEnd: 20,
-    flexDirection: 'column'
+    backgroundColor: '#FCFCFC',
+    justifyContent: 'flex-end'
+  },
+  scroll: {
+    flex: 2,
   },
   topIndicator: {
     height: 7,
@@ -166,6 +181,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     textAlign: 'center',
     marginBottom: 20,
+    fontFamily: 'montSerrat',
+    fontWeight: 'bold',
   },
   headerText: {
     fontSize: 16,
@@ -173,12 +190,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     textAlign: 'center',
     marginBottom: 10,
+    fontFamily: 'montSerrat',
+    fontWeight: 'bold'
   },
   subText: {
     fontSize: 12,
     color: '#686868',
     alignItems: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    fontFamily: 'montSerrat'
   },
   clickableTextContainer: {
     alignItems: 'center',
@@ -190,6 +210,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#5994db',
     fontSize: 14,
+    fontFamily: 'montSerrat'
   },
   textFieldsVerticalContainer: {
     flex: 1,
@@ -222,5 +243,8 @@ const styles = StyleSheet.create({
 
 }
 });
-
+const mapStateToProps = ({ fontReducer, songsReducer }) => {
+  return { ...fontReducer, ...songsReducer};
+};
+const UploadMediaFilledScreen = connect(mapStateToProps)(UploadMediaFilledScreenContainer);
 export {UploadMediaFilledScreen};
