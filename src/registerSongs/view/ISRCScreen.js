0af5c3abfd2263f45dc4ab755a15690d63c61779
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-06-04 16:25:56 -0300

    - Updated ISRCScreen elements margins; - Updated ISRCScreen text underine;

--- src/registerSongs/view/ISRCScreen.js
+++ src/registerSongs/view/ISRCScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import { TextField } from 'react-native-material-textfield';
 import { connect } from 'react-redux';
-import { MPHeader, MPFooter } from '../../components';
+import { MPHeader, MPFooter, MPTextField } from '../../components';
 
 class ISRCScreenContainer extends React.Component {
   constructor(props){
@@ -14,28 +14,20 @@ class ISRCScreenContainer extends React.Component {
     return (
       <View style={styles.container}>
         <MPHeader back={true} onBack={this.handleBackClick} title={"Nº ISRC (código-padrão internacional de gravação)"} />
+        <ScrollView style={styles.scroll}>
         {
           this.props.fontLoaded ? (
-            <ScrollView style={styles.scroll}>
+            <View>
               <Text style={styles.textTop}>Informe o ISRC, caso a música já esteja registrada:</Text>
-              <TextField 
-              label='Nº do ISRC'
-              value={this.state.text}
-              labelFontSize={16}
-              multiline={true}
-              lineWidth={1}
-              baseColor={'#b1b1b1'} 
-              labelTextStyle={{ fontFamily: 'montSerrat' }}
-              titleTextStyle={{ fontFamily: 'montSerrat' }}/>
+              <MPTextField label={'Nº do ISRC'} value={''} />
               <View style={[styles.clickableTextContainer, {marginTop: 76}]}>
-                <Text style={styles.clickableText}>A gravação ainda nao está registrada.</Text>
-              </View>
-              <View style={[styles.clickableTextContainer, {marginTop: 20}]}>
+                <Text style={[styles.clickableText, {marginBottom: 20}]}>A gravação ainda nao está registrada.</Text>
                 <Text style={styles.clickableText}>Eu não sei o que é ISRC.</Text>
               </View>
-            </ScrollView>
+            </View>
           ) : null
         }
+        </ScrollView>
         <MPFooter/>
       </View>
     );
@@ -47,36 +39,23 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     backgroundColor: '#FCFCFC',
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
   },
   scroll: {
     flex: 2,
-    paddingStart :40,
-    paddingEnd: 40,
   },
   textTop: {
     fontSize: 16,
     color: '#686868',
-    marginBottom: 20,
-    fontFamily: 'montSerrat'
-  },
-  textInputContainer: {
-    height: 46,
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderColor: '#b1b1b1',
-    padding: 5
-  },
-  textInput: {
-    borderColor: 'transparent',
+    fontFamily: 'montSerrat',
+    marginHorizontal: 40
   },
   clickableTextContainer: {
     alignItems: 'flex-start',
-    height: 20
+    marginHorizontal: 40
   },
   clickableText: {
-    borderBottomWidth: 1,
-    borderColor: '#5994db',
+    textDecorationLine: 'underline',
     textAlign: 'center',
     color: '#5994db',
     fontSize: 14,
