1cba6ae588e3b30cf83d28125c390ca42ea91eaa
Victor Arruda Magalhães <victor@onlytech.me>
2018-06-13 15:27:47 -0300

    chat screen partially done

--- src/components/general/MPText.js
+++ src/components/general/MPText.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { applyFont } from './applyFont';
--- src/modules/notifications/view/ChatScreen.js
+++ src/modules/notifications/view/ChatScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {
   StyleSheet,
+  Text,
   View
 } from 'react-native';
 import {
   MPFooter,
   MPHeader
 } from '../../../components';
+import {
+  Bubble, 
+  GiftedChat,
+  InputToolbar,
+  Send
+} from 'react-native-gifted-chat';
+import { MPSendMessageIcon } from '../../../assets/svg'
 import { connect } from 'react-redux';
-import { GiftedChat } from 'react-native-gifted-chat';
 
 
 class ChatScreenContainer extends React.Component {
@@ -22,12 +29,11 @@ class ChatScreenContainer extends React.Component {
       messages: [
         {
           _id: 1,
-          text: 'Hello developer',
+          text: 'Gostei sim. Podemos marcar uma conversa? Gostaria de conversar com você. Acho que essa música pode funcionar muito bem em um dos meus artistas.',
           createdAt: new Date(),
           user: {
             _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
+            name: 'Marcelo Marra'
           },
         },
       ],
@@ -44,17 +50,81 @@ class ChatScreenContainer extends React.Component {
     this.props.navigation.pop();
   };
 
+  renderBubble = props => {
+    return (
+      <View>
+      {
+        this.props.fontLoaded ? (
+            <Bubble
+              {...props}
+              textStyle={{
+                left: {
+                  color: '#9B9B9B',
+                  fontFamily: 'probaProRegular',
+                  fontSize: 16
+                },
+                right: {
+                  color: '#424242',
+                  fontFamily: 'probaProRegular',
+                  fontSize: 16
+                }
+              }}
+              wrapperStyle={{
+                left: {
+                  backgroundColor: 'white',
+                  padding: 10,
+                  shadowColor: '#000000',
+                  shadowOpacity: 0.15,
+                  shadowRadius: 2,
+                  shadowOffset: {
+                    width: 1,
+                    height: 1
+                  }
+                },
+                right: {
+                  backgroundColor: '#DFDFDF',
+                  padding: 10
+                }
+              }}
+            />
+        ) : null
+      }
+      </View>
+    )
+  }
+
+  renderInputToolbar = props => {
+    return (
+      <InputToolbar
+        {...props}
+        containerStyle={{borderTopWidth: 0.5, borderTopColor: '#E6E6E6'}}
+        placeholder="Digite sua mensagem"
+      />
+    ) 
+  }
+
+  renderSend = props => {
+    return (
+      <Send {...props}>
+        <MPSendMessageIcon />
+      </Send>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MPHeader back={true} onBack={this.handleBackClick} title={""} />
-        <GiftedChat
-          messages={this.state.messages}
-          onSend={messages => this.onSend(messages)}
-          user={{
-            _id: 1,
-          }}
-        />
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            renderBubble={this.renderBubble}
+            renderSend={this.renderSend}
+            renderInputToolbar={this.renderInputToolbar}
+            user={{
+              _id: 1,
+            }}
+          />
         <MPFooter />
       </View>
     );
