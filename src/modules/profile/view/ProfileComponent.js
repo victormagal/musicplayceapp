4196782ea31f8497c182e31f3f5c72b02a323448
Jhonatas Santos <jhonatasmartins@hotmail.com>
2018-06-20 10:19:39 -0300

    fix

--- src/modules/profile/view/ProfileComponent.js
+++ src/modules/profile/view/ProfileComponent.js
@@ -1,83 +1,98 @@
 import React from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import {View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import {LinearGradient} from 'expo';
 import PropTypes from 'prop-types';
-import { MPTabBar, MPProfileInfo, MPShowLanguages, MPHeader, MPFooter, MPFollowButton, ProfileIndicatorCE, MPAddSongButton, MPAddChangePhoto, MPText, MPUploadFirstSong, MPShowFollowers, MPShowAgencies, MPReportProfile, MPShowFolderSongs } from '../../../components/';
-import { MPProfileArrowIcon } from '../../../assets/svg/'
-import { MPUpgradeButton } from '../../../components/profile/MPUpgradeButton';
+import {
+  MPTabBar,
+  MPProfileInfo,
+  MPShowLanguages,
+  MPHeader,
+  MPFollowButton,
+  ProfileIndicatorCE,
+  MPAddSongButton,
+  MPAddChangePhoto,
+  MPUploadFirstSong,
+  MPShowFollowers,
+  MPShowAgencies,
+  MPReportProfile,
+  MPShowFolderSongs
+} from '../../../components/';
+import {MPProfileArrowIcon} from '../../../assets/svg/'
+import {MPUpgradeButton} from '../../../components/profile/MPUpgradeButton';
 
 
 class ProfileComponent extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            profileOption: true,
-        }
-    }
 
-    toggleState = () => {
-        this.setState({[profileOption]: !this.state.profileOption});
-      }
+  scrollViewRef = null;
 
-    render() {
-        let {profile} = this.props;
+  constructor(props) {
+    super(props);
+    this.scrollViewRef = React.createRef();
+  }
 
-        return (
-            <ScrollView style={styles.container}>
-                <LinearGradient
-                    colors={["rgba(0, 0, 0, 0.2)", "#e13223"]}
-                    style={styles.gradient}>
-                    <MPHeader transparent={true} title={""}/>
-                    {
-                        profile.visiting ? (
-                            <MPFollowButton isFollowing={profile.isFollowing} />
-                        ) : (
-                            <MPAddChangePhoto hasPhoto={profile.hasPhoto} />
-                        )
-                    }
-                    <MPProfileInfo profile={profile} />
-                    <View style={{flexDirection: 'row', marginBottom: 20, marginHorizontal: 20}}>
-                        <ProfileIndicatorCE style={{ flex: 1}} title="Indicações Feitas" subtitle="Explore" />
-                        <ProfileIndicatorCE style={{ flex: 1}} title="Seguidores" subtitle="Convide seus amigos"/>
-                    </View>
-                    {
-                        profile.languages != '' ? (
-                            <MPShowLanguages languages={profile.languages} />
-                        ) : null
-                    }
-                    {
-                        profile.agencies != '' ? (
-                            <MPShowAgencies agencies={profile.agencies} isArtist={profile.isArtist}/>
-                        ) : null
-                    }
-                    <View style={{alignSelf: 'center', justifyContent: 'center', marginBottom: 20,}} onPress={this.toggleState.bind(this)}>
-                        <MPProfileArrowIcon />
-                    </View>
-                </LinearGradient>
-                {
-                    this.state.profileOption ? (
-                        <MPTabBar firstTabTitle={'MINHAS MÚSICAS'} secondTabTitle={"MÚSICAS SALVAS"}>
-                            <View style={{flex:1}}>
-                                {
-                                    profile.song == '' ? (
-                                        <MPUploadFirstSong />
-                                    ) : (
-                                        <MPUpgradeButton song={profile.song} />
-                                    )
-                                }
-                                <MPShowFollowers />
-                                <MPReportProfile />
-                            </View>
-                            <View style={{flex:1}}>
-                                <MPShowFolderSongs folderName={'Inspirações rock'} edit={true}/>
-                            </View>
-                        </MPTabBar>
-                    ) : null
-                }
-                <MPAddSongButton isColored={true} />
-            </ScrollView>
-        );
-    }
+  handleScrollEnd = () => {
+    this.scrollViewRef.current.scrollToEnd();
+  };
+
+  render() {
+    let {profile} = this.props;
+
+    return (
+      <ScrollView style={styles.container} ref={this.scrollViewRef}>
+        <View style={styles.linearContainer}>
+          <LinearGradient
+            colors={["rgba(0, 0, 0, 0.2)", "#e13223"]}
+            style={styles.gradient}>
+            <MPHeader transparent={true} title={""}/>
+            {
+              profile.visiting ? (
+                  <MPFollowButton isFollowing={profile.isFollowing}/>
+                ) : (
+                  <MPAddChangePhoto hasPhoto={profile.hasPhoto}/>
+                )
+            }
+            <MPProfileInfo profile={profile}/>
+            <View style={{flexDirection: 'row', marginBottom: 20, marginHorizontal: 20}}>
+              <ProfileIndicatorCE style={{flex: 1}} title="Indicações Feitas"  subtitle="Explore"/>
+              <ProfileIndicatorCE style={{flex: 1}} title="Seguidores" subtitle="Convide seus amigos"/>
+            </View>
+            {
+              profile.languages != '' ? (
+                  <MPShowLanguages languages={profile.languages}/>
+                ) : null
+            }
+            {
+              profile.agencies != '' ? (
+                  <MPShowAgencies agencies={profile.agencies} isArtist={profile.isArtist}/>
+                ) : null
+            }
+
+            <TouchableOpacity style={{alignSelf: 'center', justifyContent: 'center', marginBottom: 20}} onPress={this.handleScrollEnd}>
+              <MPProfileArrowIcon />
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
+
+        <MPTabBar firstTabTitle={'MINHAS MÚSICAS'} secondTabTitle={"MÚSICAS SALVAS"}>
+          <View style={{flex: 1}}>
+            {
+              profile.song == '' ? (
+                  <MPUploadFirstSong />
+                ) : (
+                  <MPUpgradeButton song={profile.song}/>
+                )
+            }
+            <MPShowFollowers />
+            <MPReportProfile />
+          </View>
+          <View style={{flex: 1}}>
+            <MPShowFolderSongs folderName={'Inspirações rock'} edit={true}/>
+          </View>
+        </MPTabBar>
+        <MPAddSongButton isColored={true}/>
+      </ScrollView>
+    );
+  }
 }
 
 ProfileComponent.propTypes = {
@@ -85,17 +100,19 @@ ProfileComponent.propTypes = {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 2,
-        backgroundColor: '#000',
-    },
-    gradient: {
-        flex: 1,
-        alignContent: 'flex-start',
-    },
-    button: {
-        width: 230
-    }
+  container: {
+    flex: 2
+  },
+  linearContainer: {
+    backgroundColor: '#000',
+  },
+  gradient: {
+    flex: 1,
+    alignContent: 'flex-start',
+  },
+  button: {
+    width: 230
+  }
 });
 
 
