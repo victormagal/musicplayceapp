1876226748816c9f05fd295e82dbcbac41ae5e60
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-07-25 12:01:35 -0300

    inital updated to playercomponent with song

--- src/modules/player/view/player/PlayerComponent.js
+++ src/modules/player/view/player/PlayerComponent.js
@@ -103,6 +103,12 @@ class PlayerComponent extends React.Component {
     this.props.navigation.navigate('IndicateSongFullScreen');
   };
 
+  handleSongDate = (songDate) => {
+    let date = songDate.split(" ")[0].split('-').reverse().join('/');
+    let time = songDate.split(" ")[1].slice(0, 5);
+    return `${date} às ${time}`;
+  }
+
   handleSaveSong = () => {
     this.setState({playerVisible: false});
     this.props.navigation.navigate('playerSaveSong');
@@ -223,6 +229,8 @@ class PlayerComponent extends React.Component {
   }
 
   renderMain() {
+    let {song} = this.props;
+    console.log(song);
     return (
       <MPFade style={styles.modalContent} visible={this.state.showPlayer}>
         <ScrollView style={styles.flexOne}>
@@ -255,11 +263,11 @@ class PlayerComponent extends React.Component {
 
               <View style={styles.row}>
                 <MPPlayIcon style={styles.musicPlayIcon}/>
-                <MPText style={styles.musicTitleText}>Tocando em Frente</MPText>
+                <MPText style={styles.musicTitleText}>{ song ? song.name : 'Tocando em Frente'}</MPText>
               </View>
 
-              <MPText style={styles.musicUploadDate}>10/05/2018 às 13:49</MPText>
-              <MPText style={styles.musicMessage}>Escute esta música de tal tal jeito.</MPText>
+              <MPText style={styles.musicUploadDate}>{song ? this.handleSongDate(song.created_at) : '10/05/2018 às 13:49'}</MPText>
+              <MPText style={styles.musicMessage}>{song? this.song.description : 'Escute esta música de tal tal jeito.'</MPText>
 
               <MPText style={styles.compositorTitle}>COMPOSITOR</MPText>
               <MPText style={styles.compositorText}>Almir Sater</MPText>
