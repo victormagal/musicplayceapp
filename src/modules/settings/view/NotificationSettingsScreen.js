f1415d0797bc2092708bb8c05289c9edde1ec531
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-08-06 16:37:02 -0300

    Implemented notificationSettings PATCH on all MPSwitch

--- src/modules/settings/view/NotificationSettingsScreen.js
+++ src/modules/settings/view/NotificationSettingsScreen.js
@@ -68,7 +68,7 @@ class NotificationSettingsScreenContainer extends React.Component {
       support_sms,
       support_unit_frequency,
     } = this.state.notificationSettings;
-    console.log(this.state.notificationSettings);
+    // console.log(this.state.notificationSettings)
     return (
       <View style={styles.parent}>
         <MPHeader back={true} onBack={this.handleBack} title={"Preferências de notificação"} />
@@ -79,42 +79,42 @@ class NotificationSettingsScreenContainer extends React.Component {
               <MPText style={styles.paragraph}>Fique sabendo quando entrarem em contato com você</MPText>
             </View>
             <MPSwitch name={'interested_email'} value={!!interested_email} label={"E-mail"} onChangeSwitch={this.onChangeSwitch} />
-            <MPSwitch name={'interested_push'} value={!!interested_push} label={"Celular / tablet"} />
-            <MPSwitch name={'interested_sms'} value={!!interested_sms} label={"SMS"} />
+            <MPSwitch name={'interested_push'} value={!!interested_push} label={"Celular / tablet"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'interested_sms'} value={!!interested_sms} label={"SMS"} onChangeSwitch={this.onChangeSwitch} />
             <View style={styles.separator} />
             <View style={styles.boxText}>
               <MPText style={styles.title}>Indicaram sua música</MPText>
               <MPText style={styles.paragraph}>Fique sabendo quando indicarem suas composições</MPText>
             </View>
             {/* <MPSelect /> */}
-            <MPSwitch name={'indication_your_song_email'} value={!!indication_your_song_email}  label={"E-mail"} />
-            <MPSwitch name={'indication_your_song_push'} value={!!indication_your_song_push} label={"Celular / tablet"} />
-            <MPSwitch name={'indication_your_song_sms'} value={!!indication_your_song_sms} label={"SMS"} />
+            <MPSwitch name={'indication_your_song_email'} value={!!indication_your_song_email}  label={"E-mail"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'indication_your_song_push'} value={!!indication_your_song_push} label={"Celular / tablet"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'indication_your_song_sms'} value={!!indication_your_song_sms} label={"SMS"} onChangeSwitch={this.onChangeSwitch} />
             <View style={styles.separator} />
             <View style={styles.boxText}>
               <MPText style={styles.title}>Indicaram para você</MPText>
               <MPText style={styles.paragraph}>Fique sabendo quando indicarem músicas para você</MPText>
             </View>
             {/* <MPSelect /> */}
-            <MPSwitch name={'indication_email'} value={!!indication_email} label={"E-mail"} />
-            <MPSwitch name={'indication_push'} value={!!indication_push} label={"Celular / tablet"} />
-            <MPSwitch name={'indication_sms'} value={!!indication_sms} label={"SMS"} />
+            <MPSwitch name={'indication_email'} value={!!indication_email} label={"E-mail"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'indication_push'} value={!!indication_push} label={"Celular / tablet"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'indication_sms'} value={!!indication_sms} label={"SMS"} onChangeSwitch={this.onChangeSwitch} />
             <View style={styles.separator} />
             <View style={styles.boxText}>
               <MPText style={styles.title}>Promoções e dicas</MPText>
               <MPText style={styles.paragraph}>Receba promoções e dicas para turbinar o seu perfil</MPText>
             </View>
-            <MPSwitch name={'promotion_email'} value={!!promotion_email} label={"E-mail"} />
-            <MPSwitch name={'promotion_push'} value={!!promotion_push} label={"Celular / tablet"} />
-            <MPSwitch name={'promotion_sms'} value={!!promotion_sms} label={"SMS"} />
+            <MPSwitch name={'promotion_email'} value={!!promotion_email} label={"E-mail"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'promotion_push'} value={!!promotion_push} label={"Celular / tablet"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'promotion_sms'} value={!!promotion_sms} label={"SMS"} onChangeSwitch={this.onChangeSwitch} />
             <View style={styles.separator} />
             <View style={styles.boxText}>
               <MPText style={styles.title}>Ajuda</MPText>
               <MPText style={styles.paragraph}>Quando entrar em contato com a gente, como prefere receber nossas respostas e avisos importantes?</MPText>
             </View>
-            <MPSwitch name={'support_email'} value={!!support_email} label={"E-mail"} />
-            <MPSwitch name={'support_push'} value={!!support_push} label={"Celular / tablet"} />
-            <MPSwitch name={'support_sms'} value={!!support_sms} label={"SMS"} />
+            <MPSwitch name={'support_email'} value={!!support_email} label={"E-mail"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'support_push'} value={!!support_push} label={"Celular / tablet"} onChangeSwitch={this.onChangeSwitch} />
+            <MPSwitch name={'support_sms'} value={!!support_sms} label={"SMS"} onChangeSwitch={this.onChangeSwitch} />
           </View>
         </ScrollView>
       </View>
