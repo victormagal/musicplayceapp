d6904721e3e123235020e10eea6cb345bb53051e
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-08-06 17:38:06 -0300

    Started adjusting folders service for new endpoints

--- src/service/FolderService.js
+++ src/service/FolderService.js
@@ -13,16 +13,39 @@ class FolderService {
         attributes: {...folder}
       }
     };
-
     return axios.post(API_FOLDER, data);
   }
 
-  static folders(){
-    return axios.get(API_FOLDER)
+  static getUserFolders(){
+    return axios.get(`${API_FOLDER}?query={"type":"userSongs"}&queryTable=folders&include=userSongs`)
                  .then(response => {
                    let {data} = response.data;
                    data = transformResponseData(data);
                    return {data};
+                 }).catch(e =>{
+                   console.log('getUserSongsFoldersError', e);
+                 });
+  }
+
+  static getFavoriteSongsFolders(){
+    return axios.get(`${API_FOLDER}?query={"type":"favoriteSongs"}&queryTable=folders&include=favoriteSongs`)
+                 .then(response => {
+                   let {data} = response.data;
+                   data = transformResponseData(data);
+                   return {data};
+                 }).catch(e =>{
+                   console.log('getFavoriteSongsFoldersError', e);
+                 });
+  }
+
+  static folders(){
+    return axios.get(`${API_FOLDER}?query={"type":"favoriteSongs"}&queryTable=folders&include=favoriteSongs`)
+                 .then(response => {
+                   let {data} = response.data;
+                   data = transformResponseData(data);
+                   return {data};
+                 }).catch(e =>{
+                   console.log('getFoldersError', e);
                  });
   }
 }
