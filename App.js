f93a04489886d6c44025d6d045a5bfbcb55d7069
Jhonatas Santos <jhonatasmartins@hotmail.com>
2018-08-15 14:28:27 -0300

    remove console.log

--- App.js
+++ App.js
@@ -109,8 +109,6 @@ export default class App extends React.Component {
   handleSongUpdateListener = (state) => {
     let currentPlayerState = state["state"];
 
-    console.log(state, RNMusicPlayer);
-
     if (RNMusicPlayer.statePlaying == currentPlayerState) {
       store.dispatch(playerSongUpdateProgress(state["progress"]));
     }else if(RNMusicPlayer.statePaused == currentPlayerState || RNMusicPlayer.stateStopped === currentPlayerState){
